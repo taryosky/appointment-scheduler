57101fbc7fdee201e2202fc922282c15db7b07d1
Azibataram Clement <clementazibataram@gmail.com>
2021-07-18 10:49:00 +0100

    add identity tables and first migration

---
+++ AppointmentScheduling/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentScheduling.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
